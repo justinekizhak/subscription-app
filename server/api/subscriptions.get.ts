60d08cc20ae09468de30ee45f42e96837f07a40c
Justine Kizhakkinedath <justine@kizhak.com>
2024-09-23 20:41:14 +0530

    refactor: remove unused subscription list file and update subscription fetching logic

--- server/api/subscriptions.get.ts
+++ server/api/subscriptions.get.ts
@@ -1,7 +1,29 @@
-import subscriptionList from "~/mocks/data/subscription-list.json";
-import { Subscription } from "~/types";
+import { z } from "zod";
+import { env } from "~/env";
+import { SubscriptionType } from "~/types";
 
-export default defineEventHandler(() => {
-  const data: Subscription[] = subscriptionList;
-  return data;
+export default defineEventHandler(async () => {
+  try {
+    const myHeaders = new Headers();
+    myHeaders.append("apiKey", env.SUPABASE_API_KEY || "");
+
+    const res = await fetch(`${env.SUPABASE_REST_URL}/subscriptions`, {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow",
+    });
+    if (!res.ok) {
+      throw new Error("Failed to fetch subscriptions");
+    }
+    const data = (await res.json()).map((item: any) => {
+      return {
+        ...item,
+        imageUrl: item.image_url,
+      };
+    });
+    return z.array(SubscriptionType).parse(data);
+  } catch (error) {
+    console.error("Error: ", error);
+    throw new Error("Failed to fetch subscriptions");
+  }
 });
