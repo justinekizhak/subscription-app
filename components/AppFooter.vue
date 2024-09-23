c009a68ca8321cbf60bef3c58463f5025e61d9b4
Justine Kizhakkinedath <justine@kizhak.com>
2024-09-23 19:45:41 +0530

    refactor: update footer and header components
    - Update AppFooter.vue to include new SVG icons for arrow, chart, home, and wallet
    - Update AppHeader.vue to display greetings, name, and initials

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,17 +1,29 @@
-
 <template>
-  <div>
-    <NuxtLink v-for="link in links" :key="link.path" :to="link.path">
-      {{  link.name }}
+  <div
+    class="flex justify-evenly border-t border-gray-400 py-3 sticky bottom-0 bg-white"
+  >
+    <NuxtLink
+      v-for="link in links"
+      :key="link.path"
+      :to="link.path"
+      class="flex flex-col items-center p-2 rounded-lg"
+    >
+      <component :is="link.icon" class="w-6 h-6" />
+      <span>
+        {{ link.name }}
+      </span>
     </NuxtLink>
   </div>
 </template>
 
-<script setup lang='ts'>
+<script setup lang="ts">
+import ChartBarSquare from "~/assets/chart-bar-square.svg";
+import HomeIcon from "~/assets/home.svg";
+import WalletIcon from "~/assets/wallet.svg";
 
 const links = [
-  { name: 'Home', path: '/' },
-  { name: 'Subscriptions', path: '/subscriptions' },
-  { name: 'Analytics', path: '/analytics' },
-]
+  { name: "Home", path: "/", icon: HomeIcon },
+  { name: "Subscriptions", path: "/subscriptions", icon: WalletIcon },
+  { name: "Analytics", path: "/analytics", icon: ChartBarSquare },
+];
 </script>
--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,16 +1,39 @@
-
 <template>
-  <div>
-    <div v-if="showGreetings">
-      Greetings
+  <div
+    class="px-8 py-3 flex justify-between items-center sticky top-0 bg-white"
+  >
+    <div class="h-12 flex items-center">
+      <div v-if="showGreetings" class="justify-between flex flex-col h-12">
+        <div class="text-sm">{{ greeting }}</div>
+        <div class="font-bold">{{ name }}</div>
+      </div>
+      <ArrowLeft v-else @click="$router.back()" class="h-6 w-6" />
     </div>
-    <div v-else @click="$router.back()">
-      <
+    <div
+      class="h-12 w-12 bg-gray-600 rounded-full text-white flex items-center justify-center"
+    >
+      {{ initials }}
     </div>
   </div>
 </template>
 
-<script setup lang='ts'>
+<script setup lang="ts">
+import ArrowLeft from "~/assets/arrow-left.svg";
+const route = useRoute();
 
-const showGreetings = ref(false)
+const showGreetings = computed(() => route.path === "/");
+
+const name = "Justine Kizhakkinedath";
+
+const initials = computed(() => {
+  const [firstName, lastName] = name.split(" ");
+  return firstName[0] + lastName[0];
+});
+
+const greeting = computed(() => {
+  const hours = new Date().getHours();
+  if (hours < 12) return "Good morning";
+  if (hours < 18) return "Good afternoon";
+  return "Good evening";
+});
 </script>
